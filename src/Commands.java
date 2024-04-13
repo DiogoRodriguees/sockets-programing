3b5b3cbeecfef227701dad64e560e6f9e60a8ffd
diogorodriguees <diogorodrigueslife@gmail.com>
2024-04-13 17:09:11 -0300

    :sparkles: change package

---
+++ src/Commands.java
@@ -0,0 +1,17 @@
+package src;
+
+public class Commands {
+    // Question 1
+    final public String connect = "CONNECT";
+    final public String success = "SUCESS";
+    final public String error = "ERROR";
+    final public String pwd = "PWD";
+    final public String chdir = "CHDIR";
+    final public String getFiles = "GETFILES";
+    final public String getDirs = "GETDIRS";
+    final public String exit = "EXIT";
+
+    final public String touch = "TOUCH";
+    final public String mkdir = "MKDIR";
+    final public String confirm = ":OK";
+}
---
+++ src/User.java
@@ -0,0 +1,11 @@
+package src;
+
+public class User {
+    public String user;
+    public String password;
+
+    public User(String user, String password) {
+        this.user = user;
+        this.password = password;
+    }
+}
